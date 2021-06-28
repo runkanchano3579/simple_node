ae30e220de9c0ca88d53a94209ecd04ded68a0b4
runkanchano3579 <redbull_aun@hotmail.com>
2021-06-28 10:45:56 +0700

    x

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+ 
+// app.get('/', function (req, res) {
+  
+  var a = 5;
+  var b = 7;
+  var c = 6;
+  if(a > b) 
+    if (b > c) document.write(a);
+    else {if(c > a) document.write(c);}
+  else if(b > c) console.log(b);
+    else document.write(c);
+  
+
+ 
+app.listen(3000)
